477f3cf81ce54a39d0e50ad474e54bf25347892c
diegosmaia <diegosmaia@gmail.com>
2019-04-18 17:06:03 -0300

    initial commit

---
+++ winRequirements/00_RemoveAutoLogon-DisablePolicy.ps1
@@ -0,0 +1,7 @@
+# this isn't needed but is a good security practice to complete
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Force
+
+$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
